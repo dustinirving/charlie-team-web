27658348234b95dbdb17fc53b219d0f96648438b
Dustin Irving <dustin.irving@gmail.com>
2024-11-16 15:33:15 -0500

    fix build errors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuIt
 import Image from 'next/image';
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-
+import classNames from 'classnames';
 const navigation = [
   { name: 'Mission', href: '/mission' },
   { name: 'Research', href: '/research' },
@@ -16,10 +16,6 @@ const navigation = [
   { name: 'Donate', href: '/donate' },
 
 ]
-
-function classNames(...classes : any) {
-  return classes.filter(Boolean).join(' ')
-}
 
 export default function Navbar() {
   const pathname = usePathname()
@@ -48,8 +44,8 @@ export default function Navbar() {
                     href={item.href}
                     aria-current={item.href === pathname  ? 'page' : undefined}
                     className={classNames(
-                      item.href === pathname ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                       'rounded-md px-3 py-2 text-sm font-medium',
+                      item.href === pathname ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                     )}
                   >
                     {item.name}
@@ -74,7 +70,7 @@ export default function Navbar() {
                 <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                   <span className="absolute -inset-1.5" />
                   <span className="sr-only">Open user menu</span>
-                  <img
+                  <Image
                     alt=""
                     src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                     className="h-8 w-8 rounded-full"
